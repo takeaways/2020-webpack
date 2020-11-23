7483a86800fe1a332b1eac499868dc53b57f6bf1
JangGeonil <wkdrjsdlf2@gmail.com>
2020-11-23 23:08:05 +0900

    Update eslint 비적용 경로 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  rules: {},
+  ignorePatterns: ["dist", "gulpfile.js", "gulp/**/*", "webpack.config.js"],
 };
